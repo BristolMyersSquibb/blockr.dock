fffc2a9d1a883c16097a3fa7cdbc059f09451cd6
DivadNojnarg <dgranjon@ymail.com>
2025-10-09 14:50:16 +0200

    raise js warnings

--- inst/assets/js/show-hide-block.js
+++ inst/assets/js/show-hide-block.js
@@ -1,6 +1,14 @@
 $(function () {
   Shiny.addCustomMessageHandler(
     'move-element', (m) => {
+      if ($(m.from).length === 0) {
+        console.warn(`move-element: 'from' selector ${m.from} not found in DOM`);
+        return;
+      };
+      if ($(m.to).length === 0) {
+        console.warn(`move-element: 'to' selector ${m.to} not found in DOM`);
+        return;
+      }
       $(m.to).append($(m.from));
     }
   )
