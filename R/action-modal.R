6305f672d64407510060fe5a6fd4a075be24d1a4
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-12-01 13:30:49 +0100

    move more utils

---
+++ R/action-modal.R
@@ -0,0 +1,272 @@
+block_modal <- function(ns, board, mode = c("append", "add")) {
+
+  mode <- match.arg(mode)
+
+  board_block_ids <- board_block_ids(board)
+  board_link_ids <- board_link_ids(board)
+
+  title <- if (mode == "append") "Append new block" else "Add new block"
+  button_label <- if (mode == "append") "Append Block" else "Add Block"
+
+  # Use different IDs for each mode to avoid conflicts
+  selection_id <- paste0(mode, "_block_selection")
+  name_id <- paste0(mode, "_block_name")
+  block_id_field <- paste0(mode, "_block_id")
+  confirm_id <- paste0(mode, "_block_confirm")
+
+  # Always visible fields
+  visible_fields <- list(
+    block_registry_selectize(ns(selection_id))
+  )
+
+  # Add block name field (visible)
+  visible_fields[[length(visible_fields) + 1]] <- textInput(
+    ns(name_id),
+    label = "User defined block title (can be changed after creation)",
+    placeholder = "Select block first"
+  )
+
+  # Advanced options (collapsible)
+  advanced_fields <- list()
+
+  # Add block input field only for append mode (in advanced options)
+  if (mode == "append") {
+    advanced_fields[[length(advanced_fields) + 1]] <- block_input_select(
+      inputId = ns("append_block_input"),
+      label = "Block input"
+    )
+  }
+
+  # Add Block ID field
+  advanced_fields[[length(advanced_fields) + 1]] <- textInput(
+    ns(block_id_field),
+    label = "Block ID",
+    value = rand_names(board_block_ids)
+  )
+
+  # Add link ID field only for append mode (in advanced options)
+  if (mode == "append") {
+    advanced_fields[[length(advanced_fields) + 1]] <- textInput(
+      ns("append_link_id"),
+      label = "Link ID",
+      value = rand_names(board_link_ids)
+    )
+  }
+
+  # Collapsible advanced options section
+  advanced_section <- div(
+    id = ns("block-advanced-options"),
+    tagList(advanced_fields)
+  )
+
+  modalDialog(
+    title = title,
+    size = "l",
+    easyClose = TRUE,
+    footer = NULL,
+    tagList(
+      css_modal_advanced(ns("block-advanced-options")),
+      visible_fields,
+      toggle_button(ns("block-advanced-options"), ns("block-advanced-toggle")),
+      advanced_section,
+      confirm_button(
+        inputId = ns(confirm_id),
+        label = button_label
+      ),
+      auto_focus_script(ns(selection_id))
+    )
+  )
+}
+
+link_modal <- function(ns, board, block_id) {
+
+  board_blocks <- board_blocks(board)
+
+  stopifnot(is_string(block_id), block_id %in% names(board_blocks))
+
+  selection_id <- "create_link"
+
+  avail <- map(
+    block_input_select,
+    board_blocks,
+    names(board_blocks),
+    MoreArgs = list(links = board_links(board), mode = "inputs")
+  )
+
+  visible_fields <- list(
+    board_select(
+      id = ns(selection_id),
+      blocks = board_blocks[lgl_ply(avail, has_length)],
+      label = "Select target block",
+      choices = NULL
+    )
+  )
+
+  toggle <- toggle_button(
+    ns("link-advanced-options"),
+    ns("link-advanced-toggle")
+  )
+
+  advanced_fields <- list(
+    block_input_select(
+      inputId = ns("add_link_input"),
+      label = "Block input"
+    ),
+    textInput(
+      ns("add_link_id"),
+      label = "Link ID",
+      value = rand_names(board_link_ids(board))
+    )
+  )
+
+  advanced_section <- div(
+    id = ns("link-advanced-options"),
+    tagList(advanced_fields)
+  )
+
+  modalDialog(
+    title = "Create new link",
+    size = "m",
+    easyClose = TRUE,
+    footer = NULL,
+    tagList(
+      css_modal_advanced(ns("link-advanced-options")),
+      visible_fields,
+      toggle,
+      advanced_section,
+      confirm_button(
+        inputId = ns("add_link_confirm"),
+        label = "Add Link"
+      ),
+      auto_focus_script(ns(selection_id))
+    )
+  )
+}
+
+stack_modal <- function(ns, board, mode = c("create", "edit"), stack = NULL,
+                        stack_id = NULL) {
+
+  mode <- match.arg(mode)
+
+  board_blocks <- board_blocks(board)
+  board_stack_ids <- board_stack_ids(board)
+  board_stacks <- board_stacks(board)
+
+  avail <- available_stack_blocks(board)
+
+  if (mode == "edit") {
+    sel <- stack_blocks(stack)
+    avail <- c(avail, sel)
+  } else {
+    sel <- NULL
+  }
+
+  board_blocks <- board_blocks[avail]
+
+  # Mode-specific values
+  title <- if (mode == "create") "Create new stack" else "Edit stack"
+  button_label <- if (mode == "create") "Create Stack" else "Update Stack"
+
+  selection_id <- if (mode == "create") {
+    "stack_block_selection"
+  } else {
+    "edit_stack_blocks"
+  }
+  name_id <- if (mode == "create") "stack_name" else "edit_stack_name"
+  stack_id_field <- "stack_id"
+  confirm_id <- if (mode == "create") "stack_confirm" else "edit_stack_confirm"
+
+  # Always visible fields
+  visible_fields <- list(
+    board_select(
+      id = ns(selection_id),
+      blocks = board_blocks,
+      label = "Select blocks to add to stack (optional)",
+      choices = NULL,
+      selected = sel,
+      multiple = TRUE
+    )
+  )
+
+  # Add stack name field (visible)
+  visible_fields[[length(visible_fields) + 1]] <- textInput(
+    ns(name_id),
+    label = if (mode == "create") {
+      "Stack name (can be changed after creation)"
+    } else {
+      "Stack name"
+    },
+    placeholder = if (mode == "create") "Enter stack name" else NULL,
+    value = if (mode == "edit") stack_name(stack) else NULL
+  )
+
+  # For edit mode, add color picker to visible fields
+  if (mode == "edit") {
+    visible_fields[[length(visible_fields) + 1]] <- shinyWidgets::colorPickr(
+      inputId = ns("edit_stack_color"),
+      label = "Stack color",
+      selected = stack_color(stack),
+      theme = "nano",
+      position = "right-end",
+      useAsButton = TRUE
+    )
+  }
+
+  # Advanced options (only for create mode)
+  toggle <- NULL
+  advanced_section <- NULL
+
+  if (mode == "create") {
+    # Advanced options toggle button
+    toggle <- toggle_button(
+      ns("stack-advanced-options"),
+      ns("stack-advanced-toggle")
+    )
+
+    # Advanced options (collapsible)
+    advanced_fields <- list()
+
+    # Add color picker field
+    advanced_fields[[length(advanced_fields) + 1]] <- shinyWidgets::colorPickr(
+      inputId = ns("stack_color"),
+      label = "Stack color",
+      selected = suggest_new_colors(
+        stack_color(board_stacks)
+      ),
+      theme = "nano",
+      position = "right-end",
+      useAsButton = TRUE
+    )
+
+    # Add Stack ID field
+    advanced_fields[[length(advanced_fields) + 1]] <- textInput(
+      ns(stack_id_field),
+      label = "Stack ID",
+      value = rand_names(board_stack_ids)
+    )
+
+    # Collapsible advanced options section
+    advanced_section <- div(
+      id = ns("stack-advanced-options"),
+      tagList(advanced_fields)
+    )
+  }
+
+  modalDialog(
+    title = title,
+    size = "l",
+    easyClose = TRUE,
+    footer = NULL,
+    tagList(
+      css_modal_advanced(ns("stack-advanced-options")),
+      visible_fields,
+      toggle,
+      advanced_section,
+      confirm_button(
+        inputId = ns(confirm_id),
+        label = button_label
+      ),
+      auto_focus_script(ns(selection_id))
+    )
+  )
+}
---
+++ R/action-utils.R
@@ -0,0 +1,243 @@
+#' Action utilities
+#'
+#' Several utility functions are exported for helping with action UI:
+#'
+#' @section `block_input_select()`:
+#' Determine input options for a block by removing inputs that are already used
+#' and also takes into account some edge-cases, such as variadic blocks. If
+#' `mode` is set as "inputs", this will return a character vector, for
+#' "create", the return value of a [shiny::selectizeInput()] call and for
+#' "update", the return value of a [shiny::updateSelectizeInput()] call.
+#'
+#' @section `board_select()`:
+#' Block selection UI, enumerating all blocks in a board is available as
+#' `board_select()`. An object that inherits from `shiny.tag` is returned, which
+#' contains the result from a [shiny::selectizeInput()] call.
+#'
+#' @param block Block object
+#' @param block_id Block ID
+#' @param links Links object
+#' @param mode Switch for determining the return object
+#' @param ... Forwarded to other methods
+#'
+#' @return See the respective sections.
+#'
+#' @rdname action
+#' @export
+block_input_select <- function(block = NULL, block_id = NULL, links = NULL,
+                               mode = c("create", "update", "inputs"), ...) {
+
+  mode <- match.arg(mode)
+
+  if (mode == "inputs") {
+    stopifnot(
+      ...length() == 0L,
+      not_null(block),
+      not_null(block_id),
+      not_null(links)
+    )
+  }
+
+  if (is.null(block_id) && is.null(links)) {
+    curr <- character()
+  } else {
+    stopifnot(is_links(links), is_string(block_id))
+    curr <- links[links$to == block_id]$input
+  }
+
+  if (is.null(block)) {
+    stopifnot(is.null(block_id), is.null(links))
+
+    inps <- c(`Select a block to populate options` = "")
+    opts <- list()
+  } else {
+    stopifnot(is_block(block))
+
+    inps <- setdiff(block_inputs(block), curr)
+
+    if (is.na(block_arity(block))) {
+      num <- suppressWarnings(as.integer(curr))
+      nna <- is.na(num)
+
+      if (all(nna)) {
+        inps <- c(inps, "1")
+      } else {
+        num <- num[!nna]
+        inps <- c(inps, as.character(min(setdiff(seq_len(max(num)), num))))
+      }
+
+      opts <- list(create = TRUE)
+    } else if (length(inps)) {
+      opts <- list()
+    } else {
+      if (mode == "inputs") {
+        return(character())
+      } else {
+        return(NULL)
+      }
+    }
+  }
+
+  if (mode == "create") {
+    selectizeInput(..., choices = inps, options = opts)
+  } else if (mode == "update") {
+    updateSelectizeInput(..., choices = inps, options = opts)
+  } else {
+    inps
+  }
+}
+
+#' @section `block_registry_selectize()`:
+#' This creates UI for a block registry selector via [shiny::selectizeInput()]
+#' and returns an object that inherits from `shiny.tag`.
+#' @param id Input ID
+#' @param blocks Character vector of block registry IDs
+#' @rdname action
+#' @export
+block_registry_selectize <- function(id, blocks = list_blocks()) {
+  options_data <- apply(
+    block_metadata(blocks),
+    1L,
+    function(blk) {
+      list(
+        value = unname(blk["id"]),
+        label = unname(blk["name"]),
+        description = unname(blk["description"]),
+        category = unname(blk["category"]),
+        package = unname(blk["package"]),
+        icon = unname(blk["icon"]),
+        color = blk_color(blk["category"]),
+        searchtext = paste(blk["name"], blk["description"], blk["package"])
+      )
+    }
+  )
+
+  tagList(
+    css_block_selectize(),
+    selectizeInput(
+      id,
+      label = "Select block to add",
+      choices = NULL,
+      options = list(
+        options = options_data,
+        valueField = "value",
+        labelField = "label",
+        searchField = c("label", "description", "searchtext"),
+        placeholder = "Type to search",
+        openOnFocus = FALSE,
+        render = js_blk_selectize_render()
+      )
+    )
+  )
+}
+
+#' @param selected Character vector of pre-selected block (registry) IDs
+#' @rdname action
+#' @export
+board_select <- function(id, blocks, selected = NULL, ...) {
+
+  meta <- blks_metadata(blocks)
+
+  bid <- names(blocks)
+  bnm <- chr_ply(blocks, block_name)
+
+  options_data <- map(
+    list,
+    value = bid,
+    label = bnm,
+    block_id = bid,
+    block_name = bnm,
+    block_type = meta$name,
+    package = meta$package,
+    category = meta$category,
+    icon = meta$icon,
+    color = meta$color,
+    searchtext = paste(bnm, bid, meta$package)
+  )
+
+  options <- list(
+    options = options_data,
+    valueField = "value",
+    labelField = "label",
+    searchField = c("label", "description", "searchtext"),
+    placeholder = "Type to search blocks...",
+    openOnFocus = FALSE,
+    plugins = list("remove_button", "drag_drop"),
+    render = js_blk_selectize_render()
+  )
+
+  if (!is.null(selected) && length(selected) > 0) {
+    options$items <- as.list(selected)
+  }
+
+  tagList(
+    css_block_selectize(),
+    tags$style(
+      HTML(
+        "/* Style the remove button */
+        .selectize-input .remove {
+          text-decoration: none !important;
+          color: #6c757d !important;
+          font-weight: normal !important;
+          border: none !important;
+          margin-left: 8px !important;
+          padding: 2px 6px !important;
+          border-radius: 3px !important;
+          transition: background-color 0.2s ease, color 0.2s ease;
+        }
+        .selectize-input .remove:hover {
+          background-color: rgba(108, 117, 125, 0.1) !important;
+          color: #495057 !important;
+        }"
+      )
+    ),
+    selectizeInput(
+      id,
+      ...,
+      selected = selected,
+      options = options
+    ),
+    if (!is.null(selected) && length(selected) > 0) {
+      tags$script(
+        HTML(
+          sprintf(
+            "$(document).ready(
+              function() {
+                function setSelectedValues() {
+                  var $select = $('#%s');
+                  if ($select.length) {
+                    var selectize = $select[0].selectize;
+                    if (selectize) {
+                      try {
+                        selectize.setValue(%s, true);
+                        return true;
+                      } catch(e) {
+                        console.log('Error setting values:', e);
+                        return false;
+                      }
+                    }
+                  }
+                  return false;
+                }
+                // Try multiple times with increasing delays
+                var attempts = 0;
+                var maxAttempts = 10;
+                var interval = setInterval(
+                  function() {
+                    attempts++;
+                    if (setSelectedValues() || attempts >= maxAttempts) {
+                      clearInterval(interval);
+                    }
+                  },
+                  300
+                );
+              }
+            );",
+            id,
+            paste0("['", paste(selected, collapse = "','"), "']")
+          )
+        )
+      )
+    }
+  )
+}
