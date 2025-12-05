6e0f66a940792cb215ce7c6551fcf5f25d49b750
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-12-05 09:29:14 +0100

    add board ui test

---
+++ tests/testthat/test-board-ui.R
@@ -0,0 +1,10 @@
+test_that("dummy board ui test", {
+
+  ui <- board_ui(
+    "test",
+    new_dock_board(blocks = c(a = new_dataset_block()))
+  )
+
+  expect_s3_class(ui, "shiny.tag.list")
+  expect_length(ui, 5L)
+})
