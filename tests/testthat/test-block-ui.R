45948bd648dee9de76c9eeedd91ba2e9b98406e3
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-12-05 14:51:08 +0100

    insert/remove panel test

--- tests/testthat/test-block-ui.R
+++ tests/testthat/test-block-ui.R
@@ -1,3 +1,52 @@
+test_that("insert/remove panel test", {
+
+  board <- new_dock_board()
+
+  testServer(
+    get_s3_method("board_server", board),
+    {
+      session$flushReact()
+      expect_null(board_update())
+
+      board_update(
+        list(
+          blocks = list(
+            add = blocks(a = new_dataset_block())
+          )
+        )
+      )
+
+      with_mocked_bindings(
+        session$flushReact(),
+        determine_panel_pos = function(dock) {
+          expect_s3_class(dock$proxy, "dock_view_proxy")
+          TRUE
+        }
+      )
+
+      expect_null(board_update())
+
+      board_update(
+        list(blocks = list(rm = "a"))
+      )
+
+      session$flushReact()
+
+      expect_null(board_update())
+    },
+    args = list(
+      x = board,
+      dock = list(
+        proxy = dockViewR::dock_view_proxy(
+          dock_id(),
+          session = MockShinySession$new()
+        )
+      )
+    )
+  )
+
+})
+
 test_that("dummy block ui test", {
 
   ui <- block_ui(
