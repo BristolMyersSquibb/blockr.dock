983d99bbe93838b8c76e404d02b9bfabaef8db2d
Christoph Sax <christoph.sax@gmail.com>
2025-12-26 11:06:16 +0100

    blockr.core::unregister_blocks

--- inst/examples/dock/app.R
+++ inst/examples/dock/app.R
@@ -8,6 +8,20 @@ options(
   "g6R.layout_on_data_change" = TRUE
 )
 
+# Unregister all blockr.core blocks except dataset_block
+# (other packages like blockr.io, blockr.dplyr, blockr.ggplot provide better alternatives)
+blockr.core::unregister_blocks(c(
+  "subset_block",
+  "merge_block",
+  "rbind_block",
+  "head_block",
+  "scatter_block",
+  "upload_block",
+  "filebrowser_block",
+  "csv_block",
+  "static_block",
+  "glue_block"
+))
 
 # Create a dock board with DAG extension (like run_app does)
 board <- new_dock_board(
