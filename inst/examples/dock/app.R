eec2bdf36bfa9df5a8bae5f5733ff8d628e49f1f
Christoph Sax <christoph.sax@gmail.com>
2026-01-06 10:24:12 +0700

    Update example app and add concept doc
    - Example now uses blockr.dag and blockr.session extensions
    - Add dev/navbar-slots-concept.md explaining the architecture

--- inst/examples/dock/app.R
+++ inst/examples/dock/app.R
@@ -1,9 +1,13 @@
 library(blockr.core)
 library(blockr.dock)
+library(blockr.dag)
+library(blockr.session)
 
 serve(
   new_dock_board(
-    extensions = new_edit_board_extension()
-  ),
-  "my_board"
+    extensions = list(
+      blockr.dag::new_dag_extension(),
+      new_session_extension()
+    )
+  )
 )
