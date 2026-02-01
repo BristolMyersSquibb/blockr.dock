196746eeaa3a96a8b834bba4cb3d18c2046aeacc
Christoph Sax <christoph.sax@gmail.com>
2026-02-01 19:26:36 +0100

    docs: add roxygen documentation for sidebar system
    - Add documentation for all exported sidebar functions
    - Add documentation for helper functions
    - Update example app

--- inst/examples/dock/app.R
+++ inst/examples/dock/app.R
@@ -1,9 +1,19 @@
-library(blockr.core)
-library(blockr.dock)
+
+# serve(
+#   new_dock_board(
+#     extensions = new_edit_board_extension()
+#   ),
+#   "my_board"
+# )
+
+
+pkgload::load_all("../blockr.dock")
+
+library(blockr)
+
 
 serve(
   new_dock_board(
-    extensions = new_edit_board_extension()
-  ),
-  "my_board"
+    extensions = new_dag_extension()
+  )
 )
