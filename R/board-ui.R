326794632be0a1ea506432162e396036a8159817
Christoph Sax <christoph.sax@gmail.com>
2026-03-24 12:03:11 +0100

    show manage_stacks

--- R/board-ui.R
+++ R/board-ui.R
@@ -29,6 +29,7 @@ board_ui.dock_board <- function(id, x, plugins = board_plugins(x),
       ),
       div(
         class = "blockr-navbar-right",
+        opt_ui_or_null("manage_stacks", plugins, x),
         tags$button(
           class = "blockr-navbar-icon-btn",
           `data-bs-toggle` = "offcanvas",
